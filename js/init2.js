90a0286846b692f1ca5c7d34a38e6945ac418fa2
Masumi Mutsuda Zapater <mutsuda@gmail.com>
2016-06-09 01:14:44 +0200

    added top filters

--- js/init2.js
+++ js/init2.js
@@ -1,19 +1,52 @@
-    $('.distribution-wrap, .year-wrap').delegate('input[type=checkbox]', 'change', function() {
-          var $lis = $('.dubbings > div'),
-            $checked = $('input:checked');  
-          if ($checked.length) {              
-                        var selector = '';
-                        $($checked).each(function(index, element){
-              if(selector === '') {
-                selector += "[data-category~='" + element.id + "']";                  
-              } else {
-                selector += ",[data-category~='" + element.id + "']";
-              }
-                        });                        
-            $lis.hide(); 
-                        console.log(selector);
-            $('.dubbings > div').hide().filter(selector).show();        
-          } else {
-            $lis.show();
-          }
-        });   
+$('.distribution-wrap, .year-wrap').delegate('select', 'change', function() 
+{
+  var $lis = $('.dubbings > div'), 
+  $checked = $('option:selected');  
+  if ($checked.length) 
+  {              
+    var type_selector = '';
+    var year_selector = '';
+    $($checked).each(function(index, element)
+    {
+      console.log(element.id);
+      if (element.id == "35mm" || element.id == "Serie")
+      { 
+        if(type_selector === '') 
+        {
+          type_selector += "[data-category~='" + element.id + "']";                  
+        } 
+        else
+        {
+          type_selector += ",[data-category~='" + element.id + "']";
+        }
+      }
+      else
+      { 
+        if(year_selector === '') 
+        {
+          year_selector += "[data-category~='" + element.id + "']";                  
+        } 
+        else
+        {
+          year_selector += ",[data-category~='" + element.id + "']";
+        }
+
+      }
+    });                        
+    $lis.hide(); 
+    console.log("type_s: "+type_selector + " " +"year_s: "+year_selector)
+    $('.dubbings > div').hide().filter(type_selector).filter(year_selector).show();        
+   
+  } 
+  else 
+  {
+    $lis.show();
+  }
+}); 
+
+
+
+
+$(document).ready(function() {
+    $('select').material_select();
+  });
