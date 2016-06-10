2ef69f9ca4434895b4cf39d08c378f7af4f27b71
Masumi Mutsuda Zapater <mutsuda@gmail.com>
2016-06-10 12:37:02 +0200

    Working on index separating code from content

--- js/init.js
+++ js/init.js
@@ -7,5 +7,6 @@
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
     $('select').material_select();
+    $('.carousel').carousel();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
