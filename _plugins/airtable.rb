4b363b2901508766af91766149aadd1a5e3cec54
Masumi Mutsuda <mutsuda@gmail.com>
2025-11-06 11:47:33 +0100

    Convert Airtable plugin to proper Jekyll generator with environment variable

--- _plugins/airtable.rb
+++ _plugins/airtable.rb
@@ -1,45 +1,67 @@
 require 'json'
 require 'airtable'
 require 'active_support/all'
-# require 'active_support/all'
 
-# Pass in api key to client
-@client = Airtable::Client.new("patBkw4PAnJZCqFIX.e389c77c1d7873d113bb431ed75de3d7ffbfd057b8141190647961ff03113ef3")
+module Jekyll
+  class AirtableGenerator < Generator
+    safe true
+    priority :low
 
-# Pass in the app key and table name
+    def generate(site)
+      begin
+        # Pass in api key to client from environment variable
+        # Set AIRTABLE_API_KEY in Netlify environment variables
+        api_key = ENV['AIRTABLE_API_KEY']
+        unless api_key
+          Jekyll.logger.warn "Airtable Generator:", "AIRTABLE_API_KEY not set, skipping Airtable data fetch"
+          return
+        end
+        client = Airtable::Client.new(api_key)
 
-@dubbings = @client.table("appsmvaOoTv8P3ypJ", "dubbings")
-@dubbings_records = @dubbings.all(:sort => ["date", :asc])
+        # Pass in the app key and table name
+        dubbings = client.table("appsmvaOoTv8P3ypJ", "dubbings")
+        dubbings_records = dubbings.all(:sort => ["date", :asc])
 
-@ads = @client.table("appsmvaOoTv8P3ypJ", "ads")
-@ads_records = @ads.all(:sort => ["date", :asc])
+        ads = client.table("appsmvaOoTv8P3ypJ", "ads")
+        ads_records = ads.all(:sort => ["date", :asc])
 
-@audiobooks = @client.table("appsmvaOoTv8P3ypJ", "audiobooks")
-@audiobooks_records = @audiobooks.all(:sort => ["date", :asc])
+        audiobooks = client.table("appsmvaOoTv8P3ypJ", "audiobooks")
+        audiobooks_records = audiobooks.all(:sort => ["date", :asc])
 
-@scores = @client.table("appsmvaOoTv8P3ypJ", "scores")
-@scores_records = @scores.all(:sort => ["name", :asc])
+        scores = client.table("appsmvaOoTv8P3ypJ", "scores")
+        scores_records = scores.all(:sort => ["name", :asc])
 
-# Change the filename here below but make sure it's in the _data folder.
-File.open("_data/dubbings.json", "w") do |f|
-    data = @dubbings_records.map { |record| record.attributes }
-    f.write(data.to_json)
-end
+        # Ensure _data directory exists
+        data_dir = File.join(site.source, "_data")
+        FileUtils.mkdir_p(data_dir) unless File.directory?(data_dir)
 
-# Change the filename here below but make sure it's in the _data folder.
-File.open("_data/ads.json", "w") do |f|
-    data = @ads_records.map { |record| record.attributes }
-    f.write(data.to_json)
-end
+        # Change the filename here below but make sure it's in the _data folder.
+        File.open(File.join(data_dir, "dubbings.json"), "w") do |f|
+          data = dubbings_records.map { |record| record.attributes }
+          f.write(data.to_json)
+        end
 
-# Change the filename here below but make sure it's in the _data folder.
-File.open("_data/audiobooks.json", "w") do |f|
-    data = @audiobooks_records.map { |record| record.attributes }
-    f.write(data.to_json)
-end
+        # Change the filename here below but make sure it's in the _data folder.
+        File.open(File.join(data_dir, "ads.json"), "w") do |f|
+          data = ads_records.map { |record| record.attributes }
+          f.write(data.to_json)
+        end
 
-# Change the filename here below but make sure it's in the _data folder.
-File.open("_data/scores.json", "w") do |f|
-    data = @scores_records.map { |record| record.attributes }
-    f.write(data.to_json)
+        # Change the filename here below but make sure it's in the _data folder.
+        File.open(File.join(data_dir, "audiobooks.json"), "w") do |f|
+          data = audiobooks_records.map { |record| record.attributes }
+          f.write(data.to_json)
+        end
+
+        # Change the filename here below but make sure it's in the _data folder.
+        File.open(File.join(data_dir, "scores.json"), "w") do |f|
+          data = scores_records.map { |record| record.attributes }
+          f.write(data.to_json)
+        end
+      rescue => e
+        Jekyll.logger.warn "Airtable Generator:", "Failed to fetch data from Airtable: #{e.message}"
+        Jekyll.logger.warn "Airtable Generator:", "Using existing data files if available"
+      end
+    end
+  end
 end
